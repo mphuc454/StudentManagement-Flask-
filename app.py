c5eec752f3f04130d857385e855638987509c261
MinhPhucHCMUAF <22130215@st.hcmuaf.edu.vn>
2026-03-02 23:13:40 +0700

    add database

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
-
-from flask import Flask, redirect, url_for, render_template, flash
+from flask import Flask, redirect, url_for, render_template
+from flask_sqlalchemy import SQLAlchemy
+from os import path
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1234@localhost:5432/MyWeb'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-programming_langue = ['HTML','CSS','JAVASCRIPT','PYTHON','SQL','FIGMA']
+
+class Skill(db.Model):
+    __tablename__ = 'skills'
+    __table_args__ = {'schema':'Portfolio'}
+
+    id = db.Column(db.Integer, primary_key = True)
+    skill_name = db.Column(db.String(255))
+
+    def __init__(self, skill_name):
+        self.skill_name = skill_name
+
+
 @app.route('/home')
 def home():
-        return render_template('index.html', context = programming_langue)
+        my_skill = Skill.query.all()
+        return render_template('index.html', skills = my_skill)
 @app.route('/skill')
 def skill():
         return render_template('skill.html')
