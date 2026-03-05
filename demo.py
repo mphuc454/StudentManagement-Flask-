2aea35b5d245a82ceab2fef7d107381d0925a7cb
MinhPhucHCMUAF <22130215@st.hcmuaf.edu.vn>
2026-03-05 22:51:34 +0700

    xoa file pycache

--- demo.py
+++ demo.py
@@ -1,3 +1,7 @@
-a = 'Long'
-if a:
-    print('your right')
+import numpy as np
+my_arr = np.array([[5,3,1,8,9],
+                   [-2,0,-2,11,3],
+                   [0,9,-8,0,18]])
+adults = my_arr[(my_arr > 4) & (my_arr < 10)]
+checked = np.where(my_arr > 15, my_arr, 0)
+print(checked)
